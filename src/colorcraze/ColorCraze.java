edeaa6c5a685cf963b01842499a8821b26b1d62d
Lauren <Lauren@104.39.179.143>
2017-10-17 13:03:16 -0400

    Added Functionality

--- src/colorcraze/ColorCraze.java
+++ src/colorcraze/ColorCraze.java
@@ -15,7 +15,12 @@ public class ColorCraze {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       MainTitle main = new MainTitle();
+       
+       main.setVisible(true);
+       
+       
+       
     }
     
 }
